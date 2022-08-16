f22995bd5b258186784d9fb3a6a1410d166ec9d2
hanzhang52 <hanzhang52@yahoo.com>
2022-08-16 12:39:53 -0500

    added gitignore files

--- professional-README-generator/generateFile.js
+++ professional-README-generator/generateFile.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 function writeFile(path, content) {
   // TODO: Create a file in the output folder
-  console.log(" I did something");
+  console.log(" I did something ");
 }
 
 module.exports = { writeFile };
--- professional-README-generator/questions.js
+++ professional-README-generator/questions.js
@@ -3,7 +3,27 @@ const questions = [
   {
     type: "input",
     name: "title",
-    message: "What is the title for this project",
+    message: "What is the title for this project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description for this project?",
+  },
+  {
+    type: "input",
+    name: "installations",
+    message: "Are there any installations required?",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
   },
 ];
 
